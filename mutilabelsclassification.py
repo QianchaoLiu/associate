73fe782013fc11e97b61402fb3ed5d364017f310
qianchaoliu <liuqianchao@163.com>
2016-09-10 19:56:05 +0800

    2nd

--- mutilabelsclassification.py
+++ mutilabelsclassification.py
@@ -1,8 +1,17 @@
 # encoding=utf-8
 # muti-labels classification for Xing at 2016/09/09
-# with libsvm to classification type of exercise
+# with svm to classification type of exercise
 import jieba
+import jieba.analyse
 import os
+import gensim
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 class svmdemo(object):
 
     def __init__(self):
@@ -21,20 +30,31 @@ class svmdemo(object):
                     wd = os.path.join(root,file)
                     with open(wd) as rf:
                         for i,line in enumerate(rf):
+                            # 剔除第一行乱码
                             if i>0:
                                 line_split = line.split('\t')
-                                pair.append([line_split[3].strip(' '),line_split[4]])
+                                # 文本,题目分类
+                                pair.append([line_split[3].strip(' ').encode('utf-8'), line_split[4]])
+                                print line_split[4]
         print len(pair),"dataset has been loaded,", "the number of types:",str(self._typecount(pair))
         return pair
 
 
 if __name__ == "__main__":
     svm = svmdemo()
+    seg_document = []
     for text_type in svm.readdat('/Users/liuqianchao/Desktop/dat'):
 
-        text = text_type[0]
-        type = text_type[1]
-        seg_list = jieba.cut(text, cut_all=False)
-        print ' '.join(seg_list)
-        print type
-        break
+        text, _type = text_type[0], text_type[1]
+        seg_list = list(jieba.cut(text, cut_all=False))
+        seg_document.append(' '.join(seg_list))
+
+        # 使用textrank提取关键词
+        # for item in jieba.analyse.textrank(text):
+        #     print item
+
+    tfidf_vectorizer = TfidfVectorizer()
+    real_vec = tfidf_vectorizer.fit_transform(seg_document)
+    seg_vec = real_vec.toarray()
+    print len(seg_vec)
+    print seg_vec[0]
